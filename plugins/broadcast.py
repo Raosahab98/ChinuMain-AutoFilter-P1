0e2c1006eae17ef6cae17595fa0dda68fcfcf2ea
Raosahab98 <iamashish986@gmail.com>
2024-10-11 05:55:47 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -4,7 +4,7 @@ import datetime
 import time
 from database.users_chats_db import db
 from info import ADMINS
-from utils import users_broadcast, groups_broadcast, temp, get_readable_time
+from utils import broadcast_messages, broadcast_messages_group, temp, get_readable_time
 import asyncio
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup 
 
@@ -53,7 +53,7 @@ async def broadcast_users(bot, message):
                 temp.USERS_CANCEL = False
                 await b_sts.edit(f"Users broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
                 return
-            sts = await users_broadcast(int(user['id']), b_msg, is_pin)
+            sts = await broadcast_messages(int(user['id']), b_msg, is_pin)
             if sts == 'Success':
                 success += 1
             elif sts == 'Error':
@@ -94,7 +94,7 @@ async def broadcast_group(bot, message):
                 temp.GROUPS_CANCEL = False
                 await b_sts.edit(f"Groups broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
                 return
-            sts = await groups_broadcast(int(chat['id']), b_msg, is_pin)
+            sts = await broadcast_messages_group(int(chat['id']), b_msg, is_pin)
             if sts == 'Success':
                 success += 1
             elif sts == 'Error':
