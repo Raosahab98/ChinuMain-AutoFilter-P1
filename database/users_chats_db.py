11157542cbb728e12a2c5053370c3c16fc7c9d33
Raosahab98 <iamashish986@gmail.com>
2024-10-16 11:40:44 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -7,11 +7,10 @@ mydb = client[DATABASE_NAME]
 class Database:
     
     def __init__(self, uri, database_name):
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(database_uri)
-        self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
         self.botcol = self.db.extrad
+        self.movies_update_channel = mydb.movies_update_channel
 
 
     def new_user(self, id, name):
